6c1c5d23b3f79c8cb51d4624a07a14a223a82de7
chenjond <113214009+chenjond@users.noreply.github.com>
2022-09-26 21:29:39 -0400

    Add email field to form

--- hello.py
+++ hello.py
@@ -14,18 +14,33 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
   name = StringField('What is your name?', validators=[DataRequired()])
+  email = StringField('What is your UofT Email address?', validators=[DataRequired()])
   submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
   form = NameForm()
+  form_submitted = False
+  valid_email = False
   if form.validate_on_submit():
+    form_submitted = True
     old_name = session.get('name')
+    old_email = session.get('email')
+
     if old_name is not None and old_name != form.name.data:
       flash('Looks like you have changed your name!')
+    if old_email is not None and old_email != form.email.data:
+      flash('Looks like you have changed your email!')
+
+    if "utoronto" in form.email.data:
+      valid_email = True
+    else:
+      valid_email = False
+    
     session['name'] = form.name.data
+    session['email'] = form.email.data
     return redirect(url_for('index'))
-  return render_template('index.html', form=form, name=session.get('name'))
+  return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'), valid_email=valid_email, form_submitted=form_submitted)
 
 @app.route('/user/<name>')
 def user(name):
